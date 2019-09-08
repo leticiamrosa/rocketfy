02f8c264378ab3d91c0c0f1df30777ad0430e037
leticiamrosa <leticiamonteiro.design@gmail.com>
2019-09-08 07:43:59 -0300

    Updates

---
+++ precache-manifest.03a98126ec067ea78384e494c0f15281.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f9f0d1137201464d7220f5dc4c3752",
+    "url": "/rocketfy/index.html"
+  },
+  {
+    "revision": "7ecd30f1eea20ef790e6",
+    "url": "/rocketfy/static/js/2.7ac5ed99.chunk.js"
+  },
+  {
+    "revision": "aec0a51417536c557fce",
+    "url": "/rocketfy/static/js/main.72df8615.chunk.js"
+  },
+  {
+    "revision": "888dea49184f14d24697",
+    "url": "/rocketfy/static/js/runtime~main.36dd39bd.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/rocketfy/precache-manifest.03a98126ec067ea78384e494c0f15281.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rocketfy/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
